15efd759f18299ff53d86c8d727f50ad2119c4f4
Henrique Pereira <henriquesti9@gmail.com>
2025-07-30 22:15:19 -0300

    Array amigos e funcao adicionarAmigo iniciada

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo(){
+    let nomeAmigo = document.querySelector('#amigo').value;
+    console.log(nomeAmigo);
+
+}
