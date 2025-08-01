8b908adc1b34065e5a4a96fe041fde140fe8d51c
Henrique Pereira <henriquesti9@gmail.com>
2025-07-31 21:52:30 -0300

    atualizando validação e inserção de nomes digitados

--- app.js
+++ app.js
@@ -2,7 +2,35 @@
 let amigos = [];
 
 function adicionarAmigo(){
-    let nomeAmigo = document.querySelector('#amigo').value;
-    console.log(nomeAmigo);
+    let nomeAmigo = document.querySelector('input').value;
+    amigos.push(nomeAmigo);
+    console.log(amigos);
+    inserirAmigosNaLista();
+    limparCampoAmigo();
+    validarEntrada();
+}
+
+function validarEntrada(){
+    if(nomeAmigo === ''){
+        alert('Insira um nome por gentileza');
+        return;
+    }
+}
+
+function inserirAmigosNaLista(){
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = '';
+
+    amigos.forEach(amigos => {
+        let itensLista = document.createElement('li');
+        itensLista.textContent = amigos;
+        listaAmigos.appendChild(itensLista);
+    });
 
 }
+
+function limparCampoAmigo(){
+    nomeAmigo = document.querySelector('input');
+    nomeAmigo.value = '';
+}
+
